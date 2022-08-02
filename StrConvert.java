776f331ab207e828b42b505f9511ffc52180712d
Snehagaikwad768 <109944120+Snehagaikwad768@users.noreply.github.com>
2022-08-02 10:22:24 +0530

    Add files via upload

---
+++ StrConvert.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+public class StrConvert {
+    public static void main(String []args){
+    String strTest = "100";
+    int iTest = Integer.parseInt(strTest);
+    System.out.println("Actual String:"+ strTest);
+    System.out.println("Converted to Int:" + iTest);
+    //This will now show some arithmetic operation
+   
+  }
+}
+
