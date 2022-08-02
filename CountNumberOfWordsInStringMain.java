a20fdfcc2d6d08f5bcf787d7dde72943fdcb4c1e
Snehagaikwad768 <109944120+Snehagaikwad768@users.noreply.github.com>
2022-08-02 10:32:35 +0530

    Add files via upload

---
+++ CountNumberOfWordsInStringMain.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+package org.arpit.java2blog.java8;
+public class CountNumberOfWordsInStringMain {
+     public static void main(String[] args) {
+        String str = "welcome to java   tutorial on Java2blog";
+ 
+        int count = 1;
+ 
+        for (int i = 0; i < str.length() - 1; i++)
+        {
+            if ((str.charAt(i) == ' ') && (str.charAt(i + 1) != ' '))
+            {
+                count++;
+            }
+        }
+        System.out.println("Number of words in a string : " + count);
+    }
+}
+ 
+
+
