ee659242a07d10fa01a04aa06bf93e81db4c81e5
Snehagaikwad768 <109944120+Snehagaikwad768@users.noreply.github.com>
2022-08-02 09:53:27 +0530

    Add files via upload

---
+++ ReverseString.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+import java.io.*;
+import java.util.Scanner;
+  
+public class ReverseString {
+    public static void main (String[] args) {
+        
+        String str= "Geeks", nstr="";
+        char ch;
+        
+      System.out.print("Original word: ");
+      System.out.println("Geeks"); //Example word
+        
+      for (int i=0; i<str.length(); i++)
+      {
+        ch= str.charAt(i); //extracts each character
+        nstr= ch+nstr; //adds each character in front of the existing string
+      }
+      System.out.println("Reversed word: "+ nstr);
+    }
+}
+ 
+
